cc09ea180225d5ffc848ace44f7d68a158656069
PeanutADi <37295205+PeanutADi@users.noreply.github.com>
2018-06-26 23:45:17 +0800

    homework 10 Online game

---
+++ homework10/Combat.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+	public bool destroyOnDeath;
+    public string type = "";
+
+    void Update()
+    {
+        if(this.type == "enemy")
+        {
+            float r = Random.Range(-5.0f, 3.0f);
+            float r2 = Random.Range(-5.0f, 3.0f);
+            if (this.transform.position.x < 8.0f && this.transform.position.z < 20.0f
+                && this.transform.position.x > -8.0f && this.transform.position.z > -20.0f) this.transform.position += new Vector3(Time.deltaTime * r, 0, Time.deltaTime * r2);
+            if (this.transform.position.x >= 8.0f) this.transform.position += new Vector3(-Time.deltaTime * 2, 0, Time.deltaTime * r2);
+            if (this.transform.position.x <= -8f) this.transform.position += new Vector3(Time.deltaTime * 2, 0, Time.deltaTime * r2);
+            if (this.transform.position.z <= -20f) this.transform.position += new Vector3(Time.deltaTime * r, 0, Time.deltaTime * 2);
+            if (this.transform.position.z >= 20f) this.transform.position += new Vector3(Time.deltaTime * r, 0, Time.deltaTime * -2);
+
+        }
+    }
+
+
+
+    [SyncVar]
+	public int health = maxHealth;
+
+	public void TakeDamage(int amount)
+	{
+        Debug.Log(destroyOnDeath);
+		if (!isServer)
+			return;
+		
+		health -= amount;
+		Debug.Log("health value = " + health.ToString());
+		if (health <= 0)
+		{
+			if (destroyOnDeath)
+			{
+				print("Destory");
+				Destroy(gameObject);
+			}
+			else
+			{
+				health = maxHealth;
+
+				// called on the server, will be invoked on the clients
+				RpcRespawn();
+			}
+		}
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// move back to zero location
+			transform.position = Vector3.zero;
+		}
+	}
+}
---
+++ homework10/PlayerMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+
+    public GameObject bulletPrefab;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void Update()
+    {
+        var hitCombat = this.GetComponent<Combat>();
+        if (hitCombat != null) hitCombat.type = "player";
+
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // This [Command] code is run on the server!
+
+        // create the bullet object locally
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward ,
+            Quaternion.identity);
+
+        bullet.transform.SetParent(this.transform);
+
+        bullet.GetComponent<Rigidbody>().velocity = new Vector3(mouseRay.direction.x * 20, 0, mouseRay.direction.z * 20);
+
+        // spawn the bullet on the clients
+        NetworkServer.Spawn(bullet);
+
+        // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
+        Destroy(bullet, 2.0f);
+    }
+
+
+
+
+}
+
